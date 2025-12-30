a8929a2f2337a5ac5f5f0c66619d6d87b1916269
Fluxx018 <alfranciz.cong@cvsu.edu.ph>
2025-12-30 13:34:31 +0800

    Delete JS-FILES directory

--- JS-FILES/subjects.js
+++
@@ -1,8 +0,0 @@
-const contains = document.querySelectorAll(".contains");
-
-contains.forEach(container => {
-    container.addEventListener("click", () => {
-        contains.forEach(cont => cont.classList.remove("selected"))
-        container.classList.add("selected")
-    })
-})
